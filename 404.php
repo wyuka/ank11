64c63d507612124248af3a04bef3d19b5209db8a
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2011-06-01 18:34:17 +0530

    added a 404 page, and a .htaccess file to configure apache server

---
+++ 404.php
@@ -0,0 +1,9 @@
+<div class="page">
+<h1>Page Not Found</h1>
+<p>
+Sorry, you were looking for <?php=getenv("REQUEST_URI")?>, but we couldn't find that page.
+</p>
+<p>
+<a href="index.php?p=home">Go back to home</a>
+</p>
+</div>
