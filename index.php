9be5671c1c0221989e86b68d83f4cdb23a1a4d1c
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2011-06-02 21:08:58 +0530

    if no argument is passed, the home page is shown.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     }
     else
     {
-        $p = "";
+        $p = "home";
     }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
